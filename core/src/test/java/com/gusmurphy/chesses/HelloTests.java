dc7fb7251cc91444affb73968d52b168acb29a8b
Augustus Murphy <gusbmurphy@gmail.com>
2025-01-04 14:22:19 -0600

    Set up JUnit

---
+++ core/src/test/java/com/gusmurphy/chesses/HelloTests.java
@@ -0,0 +1,14 @@
+package com.gusmurphy.chesses;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTests {
+
+    @Test
+    void trueIsTrue() {
+        assertTrue(true);
+    }
+
+}
