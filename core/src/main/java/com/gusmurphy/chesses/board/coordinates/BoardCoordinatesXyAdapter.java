c8a0f76ed3266e25dc876ac6ed0f0ec74c90f22a
Augustus Murphy <gusbmurphy@gmail.com>
2025-01-09 22:47:27 -0600

    Expand `BoardCoordinatesXyAdapter` to go from XY to a spot

--- core/src/main/java/com/gusmurphy/chesses/board/coordinates/BoardCoordinatesXyAdapter.java
+++ core/src/main/java/com/gusmurphy/chesses/board/coordinates/BoardCoordinatesXyAdapter.java
@@ -1,13 +1,28 @@
 package com.gusmurphy.chesses.board.coordinates;
 
+import java.util.Arrays;
+
 public class BoardCoordinatesXyAdapter {
 
     private final int x;
     private final int y;
+    private final BoardCoordinates coordinates;
 
     public BoardCoordinatesXyAdapter(BoardCoordinates coordinates) {
         this.x = coordinates.file.ordinal();
         this.y = coordinates.rank.ordinal();
+        this.coordinates = coordinates;
+    }
+
+    public BoardCoordinatesXyAdapter(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        coordinates = Arrays.stream(BoardCoordinates.values())
+            .filter(c -> c.file.ordinal() == x)
+            .filter(c -> c.rank.ordinal() == y)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Couldn't derive board coordinates from (" + x + ", " + y + ")"));
     }
 
     public int x() {
@@ -16,6 +31,10 @@ public class BoardCoordinatesXyAdapter {
 
     public int y() {
         return y;
+    }
+
+    public BoardCoordinates coordinates() {
+        return coordinates;
     }
 
 }
