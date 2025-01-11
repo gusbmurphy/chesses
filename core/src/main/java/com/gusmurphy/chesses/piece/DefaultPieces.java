d41bb6860429ffaf2917b584a6aceb067113924d
Augustus Murphy <gusbmurphy@gmail.com>
2025-01-11 10:12:55 -0600

    fixup! Create `DefaultPieces`

--- core/src/main/java/com/gusmurphy/chesses/piece/DefaultPieces.java
+++ core/src/main/java/com/gusmurphy/chesses/piece/DefaultPieces.java
@@ -1,12 +1,13 @@
 package com.gusmurphy.chesses.piece;
 
-import static com.gusmurphy.chesses.player.PlayerColor.*;
+import com.gusmurphy.chesses.player.PlayerColor;
 
 import static com.gusmurphy.chesses.board.Direction.*;
 
 public class DefaultPieces {
 
-    public static final Piece WHITE_KING = new Piece(WHITE, new LinearMovementStrategy(every(), 1));
-    public static final Piece BLACK_KING = new Piece(BLACK, new LinearMovementStrategy(every(), 1));
+    public static Piece king(PlayerColor color) {
+        return new Piece(color, new LinearMovementStrategy(every(), 1));
+    }
 
 }
