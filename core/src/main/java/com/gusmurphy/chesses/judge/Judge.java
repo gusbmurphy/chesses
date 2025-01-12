6c8d24d454851d38555888c97d119523d91d4e05
Augustus Murphy <gusbmurphy@gmail.com>
2025-01-12 12:44:30 -0600

    Remove unused imports from `Judge`

--- core/src/main/java/com/gusmurphy/chesses/judge/Judge.java
+++ core/src/main/java/com/gusmurphy/chesses/judge/Judge.java
@@ -3,11 +3,9 @@ package com.gusmurphy.chesses.judge;
 import com.gusmurphy.chesses.board.BoardState;
 import com.gusmurphy.chesses.piece.Piece;
 import com.gusmurphy.chesses.board.coordinates.BoardCoordinates;
-import com.gusmurphy.chesses.piece.PieceColorAndMovement;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 public class Judge {
 
