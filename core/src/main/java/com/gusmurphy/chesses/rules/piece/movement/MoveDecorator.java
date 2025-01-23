d5a28279776794b1958b1147888fbb981f560588
Augustus Murphy <gusbmurphy@gmail.com>
2025-01-22 19:51:00 -0600

    Create `MoveDecorator`

---
+++ core/src/main/java/com/gusmurphy/chesses/rules/piece/movement/MoveDecorator.java
@@ -0,0 +1,31 @@
+package com.gusmurphy.chesses.rules.piece.movement;
+
+import com.gusmurphy.chesses.rules.board.coordinates.BoardCoordinates;
+import com.gusmurphy.chesses.rules.piece.Piece;
+
+import java.util.Optional;
+
+public abstract class MoveDecorator implements Move {
+
+    private Move wrappedMove;
+
+    MoveDecorator(Move move) {
+        wrappedMove = move;
+    }
+
+    @Override
+    public BoardCoordinates spot() {
+        return wrappedMove.spot();
+    }
+
+    @Override
+    public Optional<Move> next() {
+        return wrappedMove.next();
+    }
+
+    @Override
+    public Optional<Piece> takes() {
+        return wrappedMove.takes();
+    }
+
+}
