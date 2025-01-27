5f566a549603530d5b795d9ef70cc9dd9fd108bb
Augustus Murphy <gusbmurphy@gmail.com>
2025-01-26 20:01:47 -0600

    Add WIP "checkmate" text to `MatchScreen`

--- core/src/main/java/com/gusmurphy/chesses/MatchScreen.java
+++ core/src/main/java/com/gusmurphy/chesses/MatchScreen.java
@@ -1,27 +1,39 @@
 package com.gusmurphy.chesses;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.gusmurphy.chesses.rules.board.BoardOnScreen;
 import com.gusmurphy.chesses.rules.board.StartingBoards;
+import com.gusmurphy.chesses.rules.judge.GameOverEvent;
+import com.gusmurphy.chesses.rules.judge.GameOverEventType;
+import com.gusmurphy.chesses.rules.judge.GameOverListener;
 
-public class MatchScreen extends BaseScreen {
+public class MatchScreen extends BaseScreen implements GameOverListener {
 
     private final SpriteBatch spriteBatch;
+    private final BitmapFont font;
     private final ShapeRenderer shapeRenderer;
     private final FitViewport viewport;
+    private final ScreenViewport fontViewport;
 
     private final BoardOnScreen boardOnScreen;
+
+    private boolean checkmate = false;
 
     public MatchScreen(final ChessesGame game) {
         spriteBatch = game.getSpriteBatch();
         shapeRenderer = game.getShapeRenderer();
         viewport = game.getViewport();
+        fontViewport = new ScreenViewport();
+        font = new BitmapFont();
 
-        boardOnScreen = new BoardOnScreen(StartingBoards.regular(), game);
+        boardOnScreen = new BoardOnScreen(StartingBoards.rookRoller(), game);
+        boardOnScreen.getJudge().subscribeToGameOver(this);
     }
 
     @Override
@@ -37,6 +49,21 @@ public class MatchScreen extends BaseScreen {
         shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
 
         boardOnScreen.draw();
+
+        if (checkmate) {
+            fontViewport.apply();
+            spriteBatch.setProjectionMatrix(fontViewport.getCamera().combined);
+            spriteBatch.begin();
+            font.setColor(Color.BLACK);
+            font.draw(spriteBatch, "Checkmate!", 0, 0);
+            spriteBatch.end();
+        }
     }
 
+    @Override
+    public void onGameOverEvent(GameOverEvent event) {
+        if (event.type() == GameOverEventType.CHECKMATE) {
+            checkmate = true;
+        }
+    }
 }
