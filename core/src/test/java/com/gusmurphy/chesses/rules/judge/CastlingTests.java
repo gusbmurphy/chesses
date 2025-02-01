4c46d53a09ea7a18195dbe439885315f530b2031
Augustus Murphy <gusbmurphy@gmail.com>
2025-02-01 15:11:13 -0600

    Parameterize castling tests

--- core/src/test/java/com/gusmurphy/chesses/rules/judge/CastlingTests.java
+++ core/src/test/java/com/gusmurphy/chesses/rules/judge/CastlingTests.java
@@ -1,9 +1,15 @@
 package com.gusmurphy.chesses.rules.judge;
 
+import com.gusmurphy.chesses.rules.PlayerColor;
 import com.gusmurphy.chesses.rules.board.BoardState;
+import com.gusmurphy.chesses.rules.board.coordinates.Coordinates;
 import com.gusmurphy.chesses.rules.piece.DefaultPieceFactory;
 import com.gusmurphy.chesses.rules.piece.Piece;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.gusmurphy.chesses.rules.PlayerColor.*;
 import static com.gusmurphy.chesses.rules.board.coordinates.Coordinates.*;
@@ -11,56 +17,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CastlingTests {
 
-    @Test
-    public void castlingMovesBothTheWhiteKingAndLeftRook() {
+    @ParameterizedTest
+    @MethodSource("castlingTestCases")
+    public void castlingMovesBothTheKingAndRook(
+        PlayerColor color, Coordinates rookStartingPosition, Coordinates kingMove, Coordinates expectedRookMove
+    ) {
         DefaultPieceFactory pieceFactory = new DefaultPieceFactory();
-        Piece leftRook = pieceFactory.rook(WHITE, A1);
-        Piece king = pieceFactory.king(WHITE);
-
-        Judge judge = new Judge(new BoardState(leftRook, king));
-        judge.submitMove(king, C1);
-
-        assertEquals(C1, king.getCoordinates());
-        assertEquals(D1, leftRook.getCoordinates());
-    }
-
-    @Test
-    public void castlingMovesBothTheWhiteKingAndRightRook() {
-        DefaultPieceFactory pieceFactory = new DefaultPieceFactory();
-        Piece rightRook = pieceFactory.rook(WHITE, H1);
-        Piece king = pieceFactory.king(WHITE);
+        Piece rightRook = pieceFactory.rook(color, rookStartingPosition);
+        Piece king = pieceFactory.king(color);
 
         Judge judge = new Judge(new BoardState(rightRook, king));
-        judge.submitMove(king, G1);
+        judge.submitMove(king, kingMove);
 
-        assertEquals(G1, king.getCoordinates());
-        assertEquals(F1, rightRook.getCoordinates());
+        assertEquals(kingMove, king.getCoordinates());
+        assertEquals(expectedRookMove, rightRook.getCoordinates());
     }
 
-    @Test
-    public void castlingMovesBothTheBlackKingAndLeftRook() {
-        DefaultPieceFactory pieceFactory = new DefaultPieceFactory();
-        Piece leftRook = pieceFactory.rook(BLACK, A8);
-        Piece king = pieceFactory.king(BLACK);
-
-        Judge judge = new Judge(new BoardState(leftRook, king));
-        judge.submitMove(king, C8);
-
-        assertEquals(C8, king.getCoordinates());
-        assertEquals(D8, leftRook.getCoordinates());
-    }
-
-    @Test
-    public void castlingMovesBothTheBlackKingAndRightRook() {
-        DefaultPieceFactory pieceFactory = new DefaultPieceFactory();
-        Piece rightRook = pieceFactory.rook(BLACK, H8);
-        Piece king = pieceFactory.king(BLACK);
-
-        Judge judge = new Judge(new BoardState(rightRook, king));
-        judge.submitMove(king, G8);
-
-        assertEquals(G8, king.getCoordinates());
-        assertEquals(F8, rightRook.getCoordinates());
+    private static Stream<Arguments> castlingTestCases() {
+        return Stream.of(
+            Arguments.of(WHITE, A1, C1, D1),
+            Arguments.of(WHITE, H1, G1, F1),
+            Arguments.of(BLACK, A8, C8, D8),
+            Arguments.of(BLACK, H8, G8, F8)
+        );
     }
 
 }
