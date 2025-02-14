050e488ba08e3f130b4c126f64c63945195caf81
Augustus Murphy <gusbmurphy@gmail.com>
2025-02-13 21:42:52 -0600

    Switch back to regular board

--- core/src/main/java/com/gusmurphy/chesses/MatchScreen.java
+++ core/src/main/java/com/gusmurphy/chesses/MatchScreen.java
@@ -34,7 +34,7 @@ public class MatchScreen extends BaseScreen implements GameOverListener {
         shapeRenderer = game.getShapeRenderer();
         viewport = game.getViewport();
 
-        boardOnScreen = new BoardOnScreen(StartingBoards.rookRoller(), game);
+        boardOnScreen = new BoardOnScreen(StartingBoards.regular(), game);
         boardOnScreen.getJudge().subscribeToGameOver(this);
 
         Skin skin = new Skin(
