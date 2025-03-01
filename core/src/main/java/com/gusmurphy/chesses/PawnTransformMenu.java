7fd5531e1e8a69778fadb0cf6b10309e0b10133f
Augustus Murphy <gusbmurphy@gmail.com>
2025-03-01 10:07:37 -0600

    Remove buttons and just have a queen image button

--- core/src/main/java/com/gusmurphy/chesses/PawnTransformMenu.java
+++ core/src/main/java/com/gusmurphy/chesses/PawnTransformMenu.java
@@ -1,6 +1,9 @@
 package com.gusmurphy.chesses;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.gusmurphy.chesses.rules.judge.PawnTransformListener;
 import com.gusmurphy.chesses.rules.judge.PawnTransformReceiver;
 
@@ -9,11 +12,10 @@ public class PawnTransformMenu extends Window implements PawnTransformListener {
     public PawnTransformMenu(Skin skin) {
         super("Select new type for pawn", skin);
 
-        TextButton queenButton = new TextButton("Queen", skin);
-        TextButton rookButton = new TextButton("Rook", skin);
-        TextButton bishopButton = new TextButton("Bishop", skin);
-        TextButton knightButton = new TextButton("Knight", skin);
-        super.add(queenButton, rookButton, bishopButton, knightButton);
+        Sprite queenSprite = new Sprite(new Texture("w_queen.png"));
+        queenSprite.setSize(30, 30);
+        Button queenButton = new ImageButton(new SpriteDrawable(queenSprite));
+        super.add(queenButton);
 
         setVisible(false);
         setMovable(true);
