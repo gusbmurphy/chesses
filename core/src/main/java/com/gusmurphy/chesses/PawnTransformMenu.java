fb8e14356a10a36e9dddbf1ca074aaceecc596d4
Augustus Murphy <gusbmurphy@gmail.com>
2025-03-01 09:55:47 -0600

    Add buttons to `PawnTransformMenu`

--- core/src/main/java/com/gusmurphy/chesses/PawnTransformMenu.java
+++ core/src/main/java/com/gusmurphy/chesses/PawnTransformMenu.java
@@ -1,7 +1,6 @@
 package com.gusmurphy.chesses;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.gusmurphy.chesses.rules.judge.PawnTransformListener;
 import com.gusmurphy.chesses.rules.judge.PawnTransformReceiver;
 
@@ -9,6 +8,13 @@ public class PawnTransformMenu extends Window implements PawnTransformListener {
 
     public PawnTransformMenu(Skin skin) {
         super("Select new type for pawn", skin);
+
+        TextButton queenButton = new TextButton("Queen", skin);
+        TextButton rookButton = new TextButton("Rook", skin);
+        TextButton bishopButton = new TextButton("Bishop", skin);
+        TextButton knightButton = new TextButton("Knight", skin);
+        super.add(queenButton, rookButton, bishopButton, knightButton);
+
         setVisible(false);
         setMovable(true);
         setResizable(true);
