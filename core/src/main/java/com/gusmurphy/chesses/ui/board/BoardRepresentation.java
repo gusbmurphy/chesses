82feec886c7d58cb63831f9e9c222a43bcf26ffc
Augustus Murphy <gusbmurphy@gmail.com>
2025-03-06 21:20:53 -0600

    Extract `drawSelectionIndicatorAt`

--- core/src/main/java/com/gusmurphy/chesses/ui/board/BoardRepresentation.java
+++ core/src/main/java/com/gusmurphy/chesses/ui/board/BoardRepresentation.java
@@ -215,22 +215,24 @@ public class BoardRepresentation implements PieceSelectionListener, PieceEventLi
                     .filter(moveIndication -> moveIndication.getCoordinates() == coordinates)
                     .findFirst()
                     .map(MoveIndication::getCoordinates)
-            ).ifPresent(coordinatesOfMoveUnderCursor -> {
-                Vector2 coordinateCenter = getScreenPositionForCenterOf(coordinatesOfMoveUnderCursor);
-                Gdx.gl.glEnable(GL20.GL_BLEND);
-                Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-                shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-                float xPosition = coordinateCenter.x - SQUARE_SIZE / 2;
-                float yPosition = coordinateCenter.y - SQUARE_SIZE / 2;
-
-                shapeRenderer.setColor(1, 0, 0, 0.5f);
-                shapeRenderer.rect(xPosition, yPosition, SQUARE_SIZE, SQUARE_SIZE);
-
-                shapeRenderer.end();
-                Gdx.gl.glDisable(GL20.GL_BLEND);
-            });
+            ).ifPresent(this::drawSelectionIndicatorAt);
         }
+    }
+
+    private void drawSelectionIndicatorAt(Coordinates coordinates) {
+        Vector2 coordinateCenter = getScreenPositionForCenterOf(coordinates);
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        float xPosition = coordinateCenter.x - SQUARE_SIZE / 2;
+        float yPosition = coordinateCenter.y - SQUARE_SIZE / 2;
+
+        shapeRenderer.setColor(1, 0, 0, 0.5f);
+        shapeRenderer.rect(xPosition, yPosition, SQUARE_SIZE, SQUARE_SIZE);
+
+        shapeRenderer.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
     }
 
     private void movePieceToCoordinatesIfLegalAndClearHighlights(Piece piece, Coordinates coordinates) {
