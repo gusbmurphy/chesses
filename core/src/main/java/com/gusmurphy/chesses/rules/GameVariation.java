a5520c35dfe3b515385a98b47dc0fdf869280a35
Augustus Murphy <gusbmurphy@gmail.com>
2025-03-14 21:54:59 -0500

    Add `singlePlayer` `GameVariation`

--- core/src/main/java/com/gusmurphy/chesses/rules/GameVariation.java
+++ core/src/main/java/com/gusmurphy/chesses/rules/GameVariation.java
@@ -2,8 +2,7 @@ package com.gusmurphy.chesses.rules;
 
 import com.gusmurphy.chesses.rules.board.BoardState;
 import com.gusmurphy.chesses.rules.board.StartingBoards;
-import com.gusmurphy.chesses.rules.judge.DefaultJudge;
-import com.gusmurphy.chesses.rules.judge.Judge;
+import com.gusmurphy.chesses.rules.judge.*;
 
 public class GameVariation {
 
@@ -18,6 +17,17 @@ public class GameVariation {
     public static GameVariation standard() {
         BoardState boardState = StartingBoards.regular();
         Judge judge = new DefaultJudge(boardState);
+        return new GameVariation(boardState, judge);
+    }
+
+    public static GameVariation singlePlayer() {
+        BoardState boardState = StartingBoards.singlePlayer();
+        // Maybe we should have a JudgeBuilder...
+        Judge judge = new CheckMateRule(
+            new CheckRule(
+                new Judge(boardState)
+            )
+        );
         return new GameVariation(boardState, judge);
     }
 
