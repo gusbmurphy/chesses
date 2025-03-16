49c2af6fdb9ad4a2958f357b2dd6fde8c511e3d6
Augustus Murphy <gusbmurphy@gmail.com>
2025-03-15 20:11:16 -0500

    Remove unused imports from `GameSelectScreen`

--- core/src/main/java/com/gusmurphy/chesses/ui/GameSelectScreen.java
+++ core/src/main/java/com/gusmurphy/chesses/ui/GameSelectScreen.java
@@ -2,11 +2,9 @@ package com.gusmurphy.chesses.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.gusmurphy.chesses.ChessesGame;
 import com.gusmurphy.chesses.rules.GameVariation;
 
