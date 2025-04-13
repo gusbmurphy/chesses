74f6f0849a25f6ca833cca929e3274612bd5859e
Augustus Murphy <gusbmurphy@gmail.com>
2025-04-12 20:55:33 -0500

    Create naive `SquareDeactivationRule`

---
+++ core/src/main/java/com/gusmurphy/chesses/rules/judge/SquareDeactivationRule.java
@@ -0,0 +1,18 @@
+package com.gusmurphy.chesses.rules.judge;
+
+import com.gusmurphy.chesses.rules.PlayerColor;
+import com.gusmurphy.chesses.rules.board.square.coordinates.Coordinates;
+import com.gusmurphy.chesses.rules.piece.Piece;
+
+public class SquareDeactivationRule extends JudgeDecorator {
+    SquareDeactivationRule(Judge judge) {
+        super(judge);
+    }
+
+    @Override
+    public void submitMove(Piece piece, Coordinates coordinates) {
+        if (piece.color() == PlayerColor.WHITE) {
+            super.submitMove(piece, coordinates);
+        }
+    }
+}
---
+++ core/src/test/java/com/gusmurphy/chesses/rules/judge/SquareDeactivatingMoveTests.java
@@ -0,0 +1,29 @@
+package com.gusmurphy.chesses.rules.judge;
+
+import com.gusmurphy.chesses.rules.piece.Piece;
+import com.gusmurphy.chesses.rules.piece.PieceFactory;
+import org.junit.jupiter.api.Test;
+
+import static com.gusmurphy.chesses.rules.board.square.coordinates.Coordinates.*;
+import static com.gusmurphy.chesses.rules.PlayerColor.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SquareDeactivatingMoveTests {
+
+    @Test
+    void afterAPieceHasMadeADeactivatingMoveAnotherPieceCannotMoveThere() {
+        PieceFactory pieceFactory = new PieceFactory();
+        Piece rook = pieceFactory.rook(WHITE, C3);
+        Piece bishop = pieceFactory.bishop(BLACK, E3);
+
+        Judge baseJudge = new TestJudge();
+        Judge squareDeactivationJudge = new SquareDeactivationRule(baseJudge);
+
+        squareDeactivationJudge.submitMove(rook, C5);
+        squareDeactivationJudge.submitMove(rook, H5);
+        squareDeactivationJudge.submitMove(bishop, C5);
+
+        assertEquals(E3, bishop.getCoordinates());
+    }
+
+}
